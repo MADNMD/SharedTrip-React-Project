76608bc9be3f98bb9913291e152fccab4557f977
Mihail Donchev <mihaildonchev88@abv.bg>
2023-11-11 19:21:26 +0200

    Create databaseConfig.js

---
+++ Rest-Api/config/databaseConfig.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const { DB_CONECTION_URL } = require('./env');
+
+exports.initDB = () => {
+
+    mongoose.connection.on('open', () => console.log('Database is conected!'));
+
+    mongoose.connect(DB_CONECTION_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+}
