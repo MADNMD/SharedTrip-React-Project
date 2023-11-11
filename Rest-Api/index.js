03c8e5f1494503a49a5c3b37d815778ecae66a7e
Mihail Donchev <mihaildonchev88@abv.bg>
2023-11-11 19:20:41 +0200

    Create index.js

---
+++ Rest-Api/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+const expressConfig = require('./config/expressConfig');
+const { initDB } = require('./config/databaseConfig');
+const { PORT } = require('./config/env');
+const router = require('./routes');
+const { auth } = require('./middlewares/authMiddleware')
+const { getErrorMessage } = require('./utils/errorHellper');
+
+const app = express();
+
+expressConfig(app);
+app.use(cors({ origin: 'http://127.0.0.1:5137', credentials: true }));
+app.use(cookieParser());
+app.use(auth);
+app.use(router);
+app.use(getErrorMessage);
+
+initDB();
+
+app.listen(PORT, () => console.log(`Server is listening at port ${PORT}`))
