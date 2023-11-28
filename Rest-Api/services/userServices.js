0be7b9307c2c7ba8b6a69cdebfa5aaac34484ec2
Mihail Donchev <mihaildonchev88@abv.bg>
2023-11-28 23:08:47 +0200

    Update userServices.js

--- Rest-Api/services/userServices.js
+++ Rest-Api/services/userServices.js
@@ -7,19 +7,19 @@ const { SECRET } = require('../config/env');
 
 exports.register = async (firstname, lastname, username, email, profilePicture, telefon, password) => {
 
-    const existingUser = await User.findOne({ $or: [{ username }, { email }] }); //Проверявам дали има съществуваш потребител с този username или email;
+    // const existingUser = await User.findOne({ $or: [{ username }, { email }] }); //Проверявам дали има съществуваш потребител с този username или email;
 
-    if (existingUser.username === username) {
-        throw {
-            message: 'Username is already taken!'
-        }
-    }
+    // if (existingUser.username === username) {
+    //     throw {
+    //         message: 'Username is already taken!'
+    //     }
+    // }
 
-    if (existingUser.email === email) {
-        throw {
-            message: 'Email is already taken!'
-        }
-    }
+    // if (existingUser.email === email) {
+    //     throw {
+    //         message: 'Email is already taken!'
+    //     }
+    // }
 
     const userData = {
         firstname,
@@ -36,22 +36,27 @@ exports.register = async (firstname, lastname, username, email, profilePicture, 
 
 exports.login = async (email, password) => {
 
-    const user = await User.findOne({ email });
+    try {
+        const user = await User.findOne({ email });
 
-    if (!user) {
-        throw {
-            message: 'Invalid email or password'
+        if (!user) {
+            throw {
+                message: 'Invalid email or password'
+            }
         }
-    }
 
-    const isValid = await bcrypt.compare(password, user.password);
+        const isValid = await bcrypt.compare(password, user.password);
 
-    if (!isValid) {
-        throw {
-            message: 'Invalid email or password'
+        if (!isValid) {
+            throw {
+                message: 'Invalid email or password'
+            }
         }
+        return user;
+    } catch (error) {
+        console.error('Error during login:', error);
+        throw error;
     }
-    return user;
 }
 
 exports.createToken = (user) => {
@@ -78,27 +83,28 @@ exports.createToken = (user) => {
     return tokenPromise;
 }
 
-exports.getUser = (userId) => User.findOne({ _id: userId }, { password: 0, __v: 0 }); // да ми върнер потребителя без неговата парола;
-
+// exports.getUser = (userId) => User.findOne({ _id: userId }, { password: 0, __v: 0 }); // да ми върнер потребителя без неговата парола;
+exports.getUser = (userId) => User.findById(userId);
 exports.editUser = async (userId, userData) => {
 
-    const existingUserWithUsername = await User.findOne({ username: userData.username });
+    // const existingUserWithUsername = await User.findOne({ username: userData.username });
 
-    if (existingUserWithUsername && existingUserWithUsername._id.toString() !== userId) {
-        throw {
-            message: 'A user with this username already exists!'
-        }
-    }
+    // if (existingUserWithUsername && existingUserWithUsername._id.toString() !== userId) {
+    //     throw {
+    //         message: 'A user with this username already exists!'
+    //     }
+    // }
 
-    const existingUserWithEmail = await User.findOne({ email: userData.email });
+    // const existingUserWithEmail = await User.findOne({ email: userData.email });
 
-    if (existingUserWithEmail && existingUserWithEmail._id.toString() !== userId) {
-        throw {
-            message: 'A user with this email already exists!'
-        }
-    }
+    // if (existingUserWithEmail && existingUserWithEmail._id.toString() !== userId) {
+    //     throw {
+    //         message: 'A user with this email already exists!'
+    //     }
+    // }
 
-    return await User.findOne({ _id: userId }, { $set: userData }, { runValidators: true });
+    return await User.updateOne({ _id: userId }, { $set: userData }, { runValidators: true });
+    // return await User.findByIdAndUpdate(userId, { $set: userData }, { new: true, runValidators: true });
 }
 
 exports.deleteUser = (userId) => User.findByIdAndDelete(userId);
