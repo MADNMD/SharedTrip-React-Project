1737404ffbc6cf58fc44f9c711cb54be4f7d4767
Mihail Donchev <mihaildonchev88@abv.bg>
2023-11-28 23:07:17 +0200

    Update commentServices.js

--- Rest-Api/services/commentServices.js
+++ Rest-Api/services/commentServices.js
@@ -1,3 +1,5 @@
 const Comment = require('../models/Comments');
 
-exports.addComment = async (userId, text) => await Comment.create({ user: userId, text });
+exports.getAllComments = async (tripId) => await Comment.find({ trip: tripId }).populate('user');
+
+exports.addComment = async (userId, tripId, text) => await Comment.create({ user: userId, trip: tripId, text });
