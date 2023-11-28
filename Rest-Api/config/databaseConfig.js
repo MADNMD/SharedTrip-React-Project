30e186dd50d64a8bea070b501e6a4b34451b2870
Mihail Donchev <mihaildonchev88@abv.bg>
2023-11-28 22:59:09 +0200

    Update databaseConfig.js

--- Rest-Api/config/databaseConfig.js
+++ Rest-Api/config/databaseConfig.js
@@ -6,8 +6,5 @@ exports.initDB = () => {
 
     mongoose.connection.on('open', () => console.log('Database is conected!'));
 
-    mongoose.connect(DB_CONECTION_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
+    mongoose.connect(DB_CONECTION_URL);
 }
