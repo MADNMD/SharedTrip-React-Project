d1b58fac26376e712da08a4c21a0eecb98cd4a48
Mihail Donchev <mihaildonchev88@abv.bg>
2023-11-28 23:10:07 +0200

    Update routes.js

--- Rest-Api/routes.js
+++ Rest-Api/routes.js
@@ -2,8 +2,12 @@ const router = require('express').Router();
 
 const userController = require('./controllers/userController');
 const tripController = require('./controllers/tripController');
+const likeController = require('./controllers/likeController');
+const commentController = require('./controllers/commentController');
 
 router.use('/users', userController);
 router.use('/trips', tripController);
+router.use('/likes', likeController);
+router.use('/comments', commentController);
 
 module.exports = router;
