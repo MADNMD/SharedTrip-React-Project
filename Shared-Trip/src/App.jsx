0a889dcc73c6dde61963ee229520f40c3ebd4846
Mihail Donchev <mihaildonchev88@abv.bg>
2023-11-30 15:19:55 +0200

    Update App.jsx

--- Shared-Trip/src/App.jsx
+++ Shared-Trip/src/App.jsx
@@ -1,35 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+
+import { AuhtProvider } from "./contexts/AuthContext";
+import { TripProvider } from "./contexts/TripContext";
+import { AllTrips } from "./components/AllTrips";
+import { CreateAcount } from "./components/CreateAcount";
+import { CreateTrip } from "./components/CreateTrip";
+import { EditProfile } from "./components/EditProfile";
+import { EditTrip } from "./components/EditTrip";
+import { Footer } from "./components/Footer";
+import { Home } from "./components/Home";
+import { Login } from "./components/Login";
+import { MyTrips } from "./components/MyTrips";
+import { Navigation } from "./components/Navigation";
+import { NotFound } from "./components/NotFound";
+import { Profile } from "./components/Profile";
+import { TripDetails } from "./components/TripDetails";
+import { Logout } from "./components/Logout";
+import { LikedStatusProvider } from "./contexts/LikeContext";
+import { RouteGuard } from "./components/common/RouteGuard";
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <AuhtProvider>
+            <TripProvider>
+                <div className="site">
+                    <Navigation />
+                    <main className="site-main">
+                        <Routes >
+                            <Route path="/" element={<Home />} />
+                            <Route path="/user/login" element={<Login />} />
+                            <Route path="/user/create-account" element={<CreateAcount />} />
+                            <Route element={<RouteGuard />}>
+                                <Route path="/user/profile/:userId" element={<Profile />} />
+                                <Route path="/user/edit-profile/:userId" element={<EditProfile />} />
+                                <Route path="/trip/create-trip" element={<CreateTrip />} />
+                                <Route path="/trip/edit-trip/:tripId" element={<EditTrip />} />
+                                <Route path="/trip/my-trips" element={<MyTrips />} />
+                            </Route>
+                            <Route path="/all-trips" element={<AllTrips />} />
+                            <Route path="/trip/trip-details/:tripId" element={<TripDetails />} />
+                            <Route path="/user/logout" element={<Logout />} />
+                            <Route path="*" element={<NotFound />} />
+                        </Routes>
+                    </main>
+                    <Footer />
+                </div>
+            </TripProvider>
+        </AuhtProvider>
+    )
 }
 
-export default App
+export default App;
+
+// Друг вариант как работят гардовете
+{/* <Routes >
+    <Route path="/" element={<Home />} />
+    <Route path="/user/login" element={<Login />} />
+    <Route path="/user/create-account" element={<CreateAcount />} />
+    <Route path="/user/profile/:userId" element={<Profile />} />
+    <Route path="/user/edit-profile/:userId" element={<EditProfile />} />
+    <Route path="/trip/create-trip" element={
+        <RouteGuard>
+            <CreateTrip />
+        </RouteGuard>
+    } />
+    <Route path="/trip/edit-trip/:tripId" element={<EditTrip />} />
+    <Route path="/trip/my-trips" element={<MyTrips />} />
+    <Route path="/all-trips" element={<AllTrips />} />
+    <Route path="/trip/trip-details/:tripId" element={<TripDetails />} />
+    <Route path="/user/logout" element={<Logout />} />
+    <Route path="*" element={<NotFound />} />
+</Routes> */}
