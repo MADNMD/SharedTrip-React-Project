eac91086ca33eae8960a89d7ed92461212da4390
Mihail Donchev <mihaildonchev88@abv.bg>
2023-12-03 00:08:38 +0200

    Create Profile.jsx

---
+++ Shared-Trip/src/components/Profile/Profile.jsx
@@ -0,0 +1,82 @@
+import { Link } from "react-router-dom";
+
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styles from './Profile.module.css';
+
+import { useAuthContext } from '../../contexts/AuthContext';
+import * as authService from '../../services/authService';
+import { Loading } from "../Loading";
+import { DeleteModal } from '../DeleteModal';
+
+export const Profile = () => {
+
+    const { userId, onLogout } = useAuthContext();
+    const [profile, setProfile] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
+    const [isDeleteModal, setIsDeleteModal] = useState(false);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        authService.getUser(userId)
+            .then(userData => {
+                setProfile(userData);
+                setIsLoading(false)
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }, [userId]);
+
+    const handleDelete = () => {
+        setIsDeleteModal(true);
+    }
+
+    const handleCancelDelete = () => {
+        setIsDeleteModal(false);
+    }
+    
+    const confirmDeleteProfile = () => {
+        authService.deleteProfile(userId);
+        setIsDeleteModal(false);
+        onLogout();
+        navigate('/user/login');
+    }
+
+    return (
+        <>
+            {isLoading
+                ? < Loading />
+                : <section className={styles.profile}>
+                    <h3>Profile</h3>
+                    <div className={styles.container}>
+                        <div className={styles['profile-wrapper']}>
+                            <div className={styles['profile-media']}>
+                                <img src={profile.profilePicture} alt="profile-picture" />
+                            </div>
+                            <div className={styles['profile-content']}>
+                                <ul>
+                                    <li><span>Firstname:</span>{profile.firstname}</li>
+                                    <li><span>Lastname:</span>{profile.lastname}</li>
+                                    <li><span>Username:</span>{profile.username}</li>
+                                    <li><span>Email:</span>{profile.email}</li>
+                                    <li><span>Telefon:</span>{profile.telefon}</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className={styles['profile-btn']}>
+                            <Link to={`/user/edit-profile/${profile._id}`}><i className="fa-solid fa-square-pen"></i>Edit</Link>
+                            <Link onClick={handleDelete}><i className="fa-solid fa-trash"></i>Delete</Link>
+                            {/* <button className={styles['profile-delete-btn']} onClick={handleDelete}><i className="fa-solid fa-trash"></i>Delete</button> */}
+                        </div>
+                    </div>
+                </section>
+            }
+            {isDeleteModal
+                ? <DeleteModal isConfirm={confirmDeleteProfile} isCancel={handleCancelDelete} />
+                : null
+            }
+
+        </>
+    )
+}
