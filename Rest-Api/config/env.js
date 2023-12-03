a142dc00057fa22524ac8188797101bc16079850
Mihail Donchev <mihaildonchev88@abv.bg>
2023-12-03 12:59:10 +0200

    change mongo url

--- Rest-Api/config/env.js
+++ Rest-Api/config/env.js
@@ -1,5 +1,5 @@
 exports.PORT = 3000;
-exports.DB_CONECTION_URL = 'mongodb+srv://mihaildonchev88:277ed21bmd@dev-app-cluster.i86lglg.mongodb.net/shared-trip?retryWrites=true&w=majority';
+exports.DB_CONECTION_URL = 'mongodb+srv://mihaildonchev88:mQvMqaSVOqXR8oDb@dev-app-cluster.i86lglg.mongodb.net/shared-trip?retryWrites=true&w=majority';
 // exports.DB_CONECTION_URL = 'mongodb://localhost:27017/shared-trip';
 exports.SECRET = 'djas-jio2323-9887dasn23f321-dsdas';
 exports.SALT_ROUNDS = 10;
