416d5007ffd9ead1658b373dbff316184a4adb4f
Mihail Donchev <mihaildonchev88@abv.bg>
2023-12-04 00:22:35 +0200

    Update EditTrip.jsx

--- Shared-Trip/src/components/EditTrip/EditTrip.jsx
+++ Shared-Trip/src/components/EditTrip/EditTrip.jsx
@@ -165,7 +165,7 @@ export const EditTrip = () => {
                                         <option value="11:15">11:15</option>
                                         <option value="11:30">11:30</option>
                                         <option value="11:45">11:45</option>
-                                        <option value="12:0">12:00</option>
+                                        <option value="12:00">12:00</option>
                                         <option value="12:15">12:15</option>
                                         <option value="12:30">12:30</option>
                                         <option value="12:45">12:45</option>
@@ -173,7 +173,7 @@ export const EditTrip = () => {
                                         <option value="13:15">13:15</option>
                                         <option value="13:30">13:30</option>
                                         <option value="13:45">13:45</option>
-                                        <option value="14:0">14:00</option>
+                                        <option value="14:00">14:00</option>
                                         <option value="14:15">14:15</option>
                                         <option value="14:30">14:30</option>
                                         <option value="14:45">14:45</option>
