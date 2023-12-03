b69204b1f23015a53d72a4eb8ab0717654d8543f
Mihail Donchev <mihaildonchev88@abv.bg>
2023-12-03 15:39:40 +0200

    Update index.js

--- Rest-Api/index.js
+++ Rest-Api/index.js
@@ -14,7 +14,7 @@ const app = express();
 expressConfig(app);
 // app.use(cors({ origin: 'http://127.0.0.1:5173', credentials: true }));
 // app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
-app.use(cors({ origin: 'https://main--shared-trip.netlify.app/', credentials: true }));
+app.use(cors({ origin: 'https://shared-trip.netlify.app', credentials: true }));
 app.use(cookieParser());
 app.use(auth);
 app.use(router);
