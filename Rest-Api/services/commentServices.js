ce265bd3341c0147d92dc2c649e3b1bacae6916f
Mihail Donchev <mihaildonchev88@abv.bg>
2023-12-05 23:02:16 +0200

    Update commentServices.js

--- Rest-Api/services/commentServices.js
+++ Rest-Api/services/commentServices.js
@@ -3,3 +3,16 @@ const Comment = require('../models/Comments');
 exports.getAllComments = async (tripId) => await Comment.find({ trip: tripId }).populate('user');
 
 exports.addComment = async (userId, tripId, text) => await Comment.create({ user: userId, trip: tripId, text });
+
+exports.editComment = async (commentId, userId, newText) => {
+
+    const comment = await Comment.findById(commentId);
+    console.log(comment)
+    if (comment.user.toString() !== userId) {  // проверявам дали потребителя е създател на коментара и ползваме toString() метода защото базата връща ид-то така new ObjectId('65649ad5038d939ac79b3169');
+        throw new Error('You do not have permission to edit this comment.');
+    }
+
+    await Comment.updateOne({ _id: commentId }, { $set: { text: newText } }, { runValidators: true });
+}
+
+exports.deleteComment = async (commentId) => Comment.findByIdAndDelete(commentId);
