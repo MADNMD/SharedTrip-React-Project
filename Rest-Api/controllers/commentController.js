a57e28c99e62164370dc43046475555b407f8b44
Mihail Donchev <mihaildonchev88@abv.bg>
2023-12-05 23:03:03 +0200

    Update commentController.js

--- Rest-Api/controllers/commentController.js
+++ Rest-Api/controllers/commentController.js
@@ -4,10 +4,10 @@ const commentService = require('../services/commentServices');
 
 router.get('/get-comments/:tripId', async (req, res) => {
 
-    const  tripId  = req.params.tripId;
+    const tripId = req.params.tripId;
     try {
         const comments = await commentService.getAllComments(tripId);
-        
+
         res.status(200).json(comments);
     } catch (error) {
         console.error(error);
@@ -24,6 +24,31 @@ router.post('/add-comment', async (req, res) => {
     } catch (error) {
         console.log(error);
         res.status(500).json({ error: 'Internal Server Error' })
+    }
+});
+
+router.put('/edit-comment/:commentId', async (req, res) => {
+
+    const commentId = req.params.commentId;
+    const { userId } = req.body;
+    const editText = req.body.text;
+    try {
+        await commentService.editComment(commentId, userId, editText);
+        res.status(200).json({ message: 'Comment edited successfuly' });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'Internal service error' });
+    }
+});
+
+router.delete('/delete-comment/:commentId', async (req, res) => {
+
+    const commentId = req.params.commentId;
+    try {
+        await commentService.deleteComment(commentId);;
+        res.status(204).json({ message: 'Comment delet successfuly' })
+    } catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
     }
 });
 
